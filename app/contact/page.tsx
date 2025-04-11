187ebedf2306e73390ed5c816a65e5e22e17e02f
Vishwesh Aryan <148064350+Int-vishwesh@users.noreply.github.com>
2025-04-11 10:40:15 +0530

    updated name in contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link"
 export default function ContactPage() {
   const teamMembers = [
     {
-      name: "Anikesh Sharma",
+      name: "Anikesh Singh",
       role: "Btech CSE ",
       image: "person-placeholder.jpg",
       email: "rahul@ticketease.com",
